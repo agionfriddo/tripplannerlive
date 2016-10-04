fdf9391a9b4275bf49e2e1f5d150ab3dd0a676ef
Andy Vays <andyvays@gmail.com>
2016-10-04 18:04:22 -0400

    working hotel button

--- models/_db.js
+++ models/_db.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('postgres://localhost:5432/tripplanner', {
+var db = new Sequelize('postgres://postgres@localhost:5432/tripplanner', {
   logging: false
 });
 
